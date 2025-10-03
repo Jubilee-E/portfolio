bd5fb27e42c4ecbd599ff472ce76fba1845e027c
Jubilee-E <artem-bodrov1408@yandex.ru>
2025-10-03 22:36:01 +0300

    сделал задание 2.4

--- js/script.js
+++ js/script.js
@@ -2,31 +2,17 @@
 
 const skills = {
   data: [
-    {
-      title: "html",
-      level: 30,
-      icon: "html.svg"
-    },
-    {
-      title: "css",
-      level: 10,
-      icon: "css.svg"
-    },
-    {
-      title: "python",
-      level: 45,
-      icon: "python.svg"
-    },
-    {
-      title: "c++",
-      level: 50,
-      icon: "c++.svg"
-    }
+    { title: "html", level: 30, icon: "html.svg" },
+    { title: "css", level: 10, icon: "css.svg" },
+    { title: "python", level: 45, icon: "python.svg" },
+    { title: "c++", level: 50, icon: "c++.svg" }
   ],
 
-  generateList(parentElement) {
-    this.data.forEach(skill => {
+  sortMode: null, 
 
+  generateList(parentElement) {
+    parentElement.innerHTML = ""; 
+    this.data.forEach(skill => {
       const dt = document.createElement("dt");
       dt.classList.add("skill-item");
       dt.textContent = skill.title;
@@ -42,8 +28,64 @@ const skills = {
       dd.append(div);
       parentElement.append(dt, dd);
     });
+  },
+
+  getComparer(prop) {
+    return function(a, b) {
+      if (a[prop] > b[prop]) return 1;
+      if (a[prop] < b[prop]) return -1;
+      return 0;
+    };
+  },
+
+  sortList(type) {
+    if (this.sortMode !== type) {
+      this.data.sort(this.getComparer(type));
+      this.sortMode = type;
+    } else {
+      this.data.reverse();
+    }
+    this.generateList(skillList);
   }
 };
 
 const skillList = document.querySelector(".skill-list");
 skills.generateList(skillList);
+
+const sortBlock = document.querySelector(".skills-sort");
+sortBlock.addEventListener("click", function(event) {
+  if (event.target.nodeName === "BUTTON") {
+    const type = event.target.dataset.type;
+    skills.sortList(type);
+  }
+});
+
+
+const navBtn = document.querySelector(".nav-btn");
+const navMenu = document.querySelector(".main-nav");
+
+const menu = {
+  open() {
+    navMenu.classList.remove("main-nav_closed");
+    navBtn.classList.remove("nav-btn_open");
+    navBtn.classList.add("nav-btn_close");
+    navBtn.textContent = "Закрыть меню";
+  },
+
+  close() {
+    navMenu.classList.add("main-nav_closed");
+    navBtn.classList.remove("nav-btn_close");
+    navBtn.classList.add("nav-btn_open");
+    navBtn.textContent = "Открыть меню";
+  }
+};
+
+menu.close();
+
+navBtn.addEventListener("click", function() {
+  if (navBtn.classList.contains("nav-btn_open")) {
+    menu.open();
+  } else {
+    menu.close();
+  }
+});
